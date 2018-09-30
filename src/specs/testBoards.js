669f6837007d243e623921dd12bcfd90179068e4
Alan Russell <ajrussellaudio@gmail.com>
2018-09-30 14:53:23 +0100

    add test boards

--- src/specs/testBoards.js
+++ src/specs/testBoards.js
@@ -5,3 +5,19 @@ export const boardFromBrief = `
   [ ][ ][ ][R][Y][Y][ ]
   [ ][ ][R][Y][R][Y][ ]
   [ ][R][Y][Y][R][Y][R]`;
+
+export const horizontalYellowVictory = `
+  [ ][ ][ ][ ][ ][ ][ ]
+  [ ][ ][ ][ ][ ][ ][ ]
+  [ ][ ][ ][ ][ ][ ][ ]
+  [ ][R][ ][ ][ ][ ][ ]
+  [ ][R][Y][ ][ ][ ][ ]
+  [ ][R][Y][Y][Y][Y][ ]`;
+
+export const verticalRedVictory = `
+  [ ][ ][ ][ ][ ][ ][ ]
+  [ ][ ][ ][ ][ ][ ][ ]
+  [ ][R][ ][ ][ ][ ][ ]
+  [ ][R][Y][ ][ ][ ][ ]
+  [ ][R][Y][ ][ ][ ][ ]
+  [ ][R][Y][ ][ ][ ][ ]`;
