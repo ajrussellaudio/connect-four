300bd9e70747a55aeb14a414255e3b1d8d62ae91
Alan Russell <ajrussellaudio@gmail.com>
2018-09-30 15:05:15 +0100

    add reverse function

--- src/manipulations.js
+++ src/manipulations.js
@@ -1,3 +1,5 @@
-import { zip } from 'lodash';
+import { zip, reverse as _reverse } from 'lodash';
 
 export const transpose = array2d => zip(...array2d);
+
+export const reverse = array => _reverse(array);
--- src/specs/manipulations.spec.js
+++ src/specs/manipulations.spec.js
@@ -1,11 +1,21 @@
 import { expect } from 'chai';
 
-import { transpose } from '../manipulations';
+import { transpose, reverse } from '../manipulations';
 
-describe('transpose', () => {
-  it('turns a row-centric 2D array into a column-centric 2D array', () => {
-    const input = [[1, 2], [3, 4], [5, 6]];
-    const expected = [[1, 3, 5], [2, 4, 6]];
-    expect(transpose(input)).to.eql(expected);
+describe('manipulations', () => {
+  describe('transpose', () => {
+    it('turns a row-centric 2D array into a column-centric 2D array', () => {
+      const input = [[1, 2], [3, 4], [5, 6]];
+      const expected = [[1, 3, 5], [2, 4, 6]];
+      expect(transpose(input)).to.eql(expected);
+    });
+  });
+
+  describe('reverse', () => {
+    it('reverses the order of an array', () => {
+      const input = [1, 2, 3];
+      const expected = [3, 2, 1];
+      expect(reverse(input)).to.eql(expected);
+    });
   });
 });
