0d6b36bb07511b74fafca7637aed6ab39c9b32b1
Alan Russell <ajrussellaudio@gmail.com>
2018-10-05 11:32:42 +0100

    add initial acceptance criteria

---
+++ src/specs/checkLength.spec.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('checkLength', () => {
+  it('returns an empty array for both colours for an empty board');
+
+  it('returns an array of one coord for a board with a single token');
+
+  it(
+    'returns an array of one coord each for a board with two different tokens'
+  );
+});
