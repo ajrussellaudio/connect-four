c2ea1ac7d29407ae5441f93db96788b2d060c94d
Alan Russell <ajrussellaudio@gmail.com>
2018-10-05 12:06:55 +0100

    add initial acceptance criteria

---
+++ src/specs/winningMove.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('winningMove', () => {
+  it('returns null when no winning move possible');
+
+  it('returns array of one column number when one winning move possible');
+
+  it('returns array of two column numbers when two winning moves possible');
+});
